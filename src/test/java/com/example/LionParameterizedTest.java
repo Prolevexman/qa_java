f58b6146e48f2f55f38a8ea78f6ed8c34e322032
Prolevexman <Prolevexman@gmail.com>
2025-07-19 18:36:42 +0300

    Добавить параметризованные тесты класса Lion

--- src/test/java/com/example/LionParameterizedTest.java
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -1,4 +1,26 @@
 package com.example;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class LionParameterizedTest {
+
+    @Mock
+    Feline feline;
+
+    @ParameterizedTest("имя")
+    @CsvSource({
+            "Самец, true",
+            "Самка, false"
+    })
+    void testDoesHaveMane(String sex, boolean expected) throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(expected, lion.doesHaveMane(), "Неверное значение гривы для пола: " + sex);
+    }
 }
